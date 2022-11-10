937a5e53470c3b6ac692dcd086f894bf1fb38b21
vikashrai <vikashrai100998@gmail.com>
2022-11-10 18:32:26 +0530

    first update

---
+++ MyApi/FirstApi/Data/FirstApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using FirstApi.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstApi.Data
+{
+    public class FirstApiDbContext:DbContext
+    {
+        public FirstApiDbContext(DbContextOptions<FirstApiDbContext> options):base(options)
+        {
+
+        }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
